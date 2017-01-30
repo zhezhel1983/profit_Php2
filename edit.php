e534aa0430924848ee3fcb7319bc3fcccbb79055
Roman Zhezhel <zhezelr@gmail.com>
2017-01-30 16:55:48 +0300

    bug fix

--- edit.php
+++ edit.php
@@ -3,8 +3,6 @@
 
 require __DIR__ . '/autoload.php';
 
-if (isset($_GET['id'])) {
-    $article = \App\Models\Article::findById($_GET['id']);
-}
+$article = \App\Models\Article::findById($_GET['id']);
 
 include __DIR__ . '/EditTemplate.php';
