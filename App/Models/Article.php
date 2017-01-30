24d23ee4056a6900303020cc47b0ddaa5f06ed6a
Roman Zhezhel <zhezelr@gmail.com>
2017-01-30 16:16:50 +0300

    delete model News.php, changed model Article.php

--- App/Models/Article.php
+++ App/Models/Article.php
@@ -18,3 +18,49 @@ class Article
         return self::findLast($num);
     }
 }
+
+/**
+ * Class News
+ * @package App\Models
+ *
+ * @property \App\Models\Article $author
+ */
+class News
+    extends Model
+{
+
+    const TABLE = 'news';
+
+    public $title;
+    public $text;
+    public $author_id;
+
+    /**
+     * LAZY LOAD
+     *
+     * @param $k
+     * @return null
+     */
+    public function __get($k)
+    {
+        switch ($k) {
+            case 'author':
+                return Author::findById($this->author_id);
+                break;
+            default:
+                return null;
+        }
+    }
+
+    public function __isset($k)
+    {
+        switch ($k) {
+            case 'author':
+                return true;
+                break;
+            default:
+                return false;
+        }
+    }
+
+}
